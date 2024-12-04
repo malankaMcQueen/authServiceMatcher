7d5d16e3aa6ddbd9b21503c56c090e3f6674067a
Daniil Voropaev <vorop2004@mail.ru>
2024-12-05 02:34:45 +0300

    Add basic configuration kafka consumer

--- src/main/java/com/example/matcher/userservice/kafka/KafkaConsumerService.java
+++ src/main/java/com/example/matcher/userservice/kafka/KafkaConsumerService.java
@@ -1,23 +1,23 @@
-//package com.example.matcher.userservice.kafka;
-//
-//import com.example.matcher.userservice.aspect.AspectAnnotation;
-//import com.example.matcher.userservice.repository.UserRepository;
-//import lombok.AllArgsConstructor;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.UUID;
-//
-//@Service
-//@AllArgsConstructor
-//public class KafkaConsumerService {
-//
-//
-//    private final UserRepository userRepository;
-//    @AspectAnnotation
-//    @KafkaListener(topics = "delete_profile", groupId = "${spring.kafka.consumer.group-id}")
-//    public void listenDeleteProfile(String userId) {
-//        System.out.println("Received JSON message: " + userId);
-//        userRepository.deleteById(UUID.fromString(userId));
-//    }
-//}
+package com.example.matcher.userservice.kafka;
+
+import com.example.matcher.userservice.aspect.AspectAnnotation;
+import com.example.matcher.userservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class KafkaConsumerService {
+
+
+    private final UserRepository userRepository;
+    @AspectAnnotation
+    @KafkaListener(topics = "delete_profile", groupId = "${spring.kafka.consumer.group-id}")
+    public void listenDeleteProfile(String userId) {
+        System.out.println("Received JSON message: " + userId);
+        userRepository.deleteById(UUID.fromString(userId));
+    }
+}
