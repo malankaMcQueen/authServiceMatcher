47306b5cfff2d4699497f7fceb8048bb05dafbcc
Daniil Voropaev <vorop2004@mail.ru>
2025-01-26 04:21:46 +0300

    Add new env handling

--- src/main/java/com/example/matcher/userservice/UserServiceApplication.java
+++ src/main/java/com/example/matcher/userservice/UserServiceApplication.java
@@ -1,9 +1,8 @@
 package com.example.matcher.userservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.example.matcher.userservice.tools.EnvironmentLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,16 +13,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 public class UserServiceApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // Загружаем переменные из .env
-		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("jwt.secret.access", dotenv.get("JWT_SECRET_ACCESS"));
-		System.setProperty("jwt.secret.refresh", dotenv.get("JWT_SECRET_REFRESH"));
-		System.setProperty("spring.security.oauth2.client.registration.google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("spring.security.oauth2.client.registration.google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
-		System.setProperty("spring.mail.username", dotenv.get("SPRING_MAIL_USERNAME"));
-		System.setProperty("spring.mail.password", dotenv.get("SPRING_MAIL_PASSWORD"));
+		System.setProperty("spring.datasource.url", EnvironmentLoader.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("spring.datasource.username", EnvironmentLoader.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("spring.datasource.password", EnvironmentLoader.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("jwt.secret.access", EnvironmentLoader.get("JWT_SECRET_ACCESS"));
+		System.setProperty("jwt.secret.refresh", EnvironmentLoader.get("JWT_SECRET_REFRESH"));
+		System.setProperty("spring.security.oauth2.client.registration.google.client-id", EnvironmentLoader.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("spring.security.oauth2.client.registration.google.client-secret", EnvironmentLoader.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("spring.mail.username", EnvironmentLoader.get("SPRING_MAIL_USERNAME"));
+		System.setProperty("spring.mail.password", EnvironmentLoader.get("SPRING_MAIL_PASSWORD"));
+		System.setProperty("telegram.bot.username", EnvironmentLoader.get("TELEGRAM_BOT_USERNAME"));
+		System.setProperty("telegram.bot.token", EnvironmentLoader.get("TELEGRAM_BOT_TOKEN"));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
